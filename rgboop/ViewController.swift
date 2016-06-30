fccac3636ac73eee34d53f757c707ae74c74ed5c
Mohammed Huraib <mhuraib@Mohammeds-iMac.local>
2016-06-30 08:13:48 +0300

    completed the graphics

--- rgboop/ViewController.swift
+++ rgboop/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var attackbgImage: UIImageView!
+    @IBOutlet weak var attackButton: UIButton!
+    @IBOutlet weak var enemyImage: UIImageView!
+    @IBOutlet weak var chestButton: UIButton!
+    @IBOutlet weak var enemyHPLabel: UILabel!
+    var newEnemyHP = 100
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func attackPressed(sender: AnyObject) {
+        
+        newEnemyHP -= 15
+        if newEnemyHP > 0
+        {
+           enemyHPLabel.text = "\(newEnemyHP) HP"
+        }
+        else
+        {
+           
+           chestButton.hidden = false
+            enemyImage.hidden = true
+            attackButton.hidden = true
+            attackbgImage.hidden = true
+            enemyHPLabel.hidden = true
+           
+        }
+        
+        
+    }
+    @IBAction func chestPressed(sender: AnyObject) {
+        
+        chestButton.hidden=true
+        enemyImage.hidden=false
+        attackButton.hidden = false
+        attackbgImage.hidden = false
+        enemyHPLabel.hidden = false
+        newEnemyHP = 100
+        enemyHPLabel.text = "\(newEnemyHP) HP"
+        
+    }
+    func createNewEnemy(){
+        
+        
+    }
 
 }
 
